847bcc5594c7f4eea075d68f24b85d900ac5b399
Michael S. Collier <buckeye01@hotmail.com>
2017-01-06 16:46:14 -0500

    initial commit for presentation and demo script

---
+++ switch-container.ps1
@@ -0,0 +1,3 @@
+﻿# Switch container environment 
+# See https://stefanscherer.github.io/run-linux-and-windows-containers-on-windows-10/
+& 'C:\Program Files\Docker\Docker\DockerCli.exe' -SwitchDaemon
