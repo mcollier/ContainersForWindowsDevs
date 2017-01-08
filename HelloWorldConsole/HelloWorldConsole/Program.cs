7111807445dc792e2b32a22a21efdc4c62d879db
Michael S. Collier <buckeye01@hotmail.com>
2017-01-08 10:55:21 -0500

    initial commit

---
+++ HelloWorldConsole/HelloWorldConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**********************************");
+            Console.WriteLine($"Hello! I am running at {Environment.MachineName}.");
+
+            for (int x = 0; x <= 5; x++)
+                Console.WriteLine("**");
+
+            // insert message here
+            Console.WriteLine($"*****    Hello {args[0]}!");
+
+            for (int x = 0; x <= 5; x++)
+                Console.WriteLine("**");
+            Console.WriteLine("**********************************");
+
+            // Just pause for a bit
+            Console.ReadLine();
+        }
+    }
+}
