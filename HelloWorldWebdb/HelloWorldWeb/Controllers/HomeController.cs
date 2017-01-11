f92fc34b3a76e6edfdeae8fcbc84889c0d7dcc53
Michael S. Collier <buckeye01@hotmail.com>
2017-01-11 10:03:29 -0500

    initial commit for version that uses Docker Compose to include a database.

---
+++ HelloWorldWebdb/HelloWorldWeb/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorldWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ActionName("About")]
+        public async System.Threading.Tasks.Task<ActionResult> AboutAsync()
+        {
+            ViewBag.Message = "Your application description page.";
+            string connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING") ??
+                                        ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+            using (SqlConnection sqlConn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    sqlConn.Open();
+
+                    using (SqlCommand sqlCmd = new SqlCommand("SELECT Top 1 (ColA) FROM dbo.Table01", sqlConn))
+                    {
+                        var result = await sqlCmd.ExecuteScalarAsync() as string;
+
+                        ViewData["result"] = result;
+                    }
+                }
+                finally
+                {
+                    if (sqlConn != null)
+                    {
+                        sqlConn.Close();
+                    }
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ HelloWorldWebdb/README.md
@@ -0,0 +1,2 @@
+# Containers for Windows Developers
+Example using Docker Compose to include a SQL Server database in the solution.
