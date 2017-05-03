e09424efdee6dce771b6148e487030528d7393c0
Michael S. Collier <buckeye01@hotmail.com>
2017-05-03 16:19:59 -0400

    Starting to update for StirTrek 2017.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# ContainersForWindowsDevs
+# Containers for Windows Developers
 Presentation on using Windows Containers.
+
+This presentation has been given at the following conferences:
+* CodeMash 2017
+* StirTrek 2017 (May 5, 2017)
--- demo-script.ps1
+++ demo-script.ps1
@@ -1,3 +1,4 @@
+<#
 ## Demo Reset
 docker rm -f $(docker ps -a -q)
 
@@ -9,19 +10,20 @@ docker pull microsoft/dotnet-samples:dotnetapp-nanoserver
 docker pull microsoft/iis
 docker pull microsoft/aspnet
 docker pull microsoft/mssql-server-windows
+#>
 
 ##1 - Show Docker running on Windows 10
 
 # Show the Client and Server version for Docker.
 docker version
 	
-# Show the Server OS/Arch setting – either ‘windows/amd64’ or ‘linux/amd64’.
+# Show the Server OS/Arch setting ï¿½ either ï¿½windows/amd64ï¿½ or ï¿½linux/amd64ï¿½.
 & 'C:\Program Files\Docker\Docker\DockerCli.exe' -SwitchDaemon	
 
 # Show the Server OS/Arch setting has changed.
 docker version
 	
-# Switch to Hyper-V Manager	Show the ‘MobyLinuxVM’ running.
+# Switch to Hyper-V Manager	Show the ï¿½MobyLinuxVMï¿½ running.
 
 # Switch back to being able to run Windows containers.
 & 'C:\Program Files\Docker\Docker\DockerCli.exe' -SwitchDaemon	
@@ -36,7 +38,7 @@ docker pull microsoft/dotnet-samples:dotnetapp-nanoserver
 docker rmi microsoft/iis	
 
 
-##2 – Manage Containers
+##2 ï¿½ Manage Containers
 
 # Run a container
 docker run microsoft/dotnet-samples:dotnetapp-nanoserver	
@@ -45,23 +47,23 @@ docker run microsoft/dotnet-samples:dotnetapp-nanoserver
 docker ps -a	
 
 # Run an existing, stopped container
-$containerID=(docker ps -a |Out-GridView -PassThru).Substring(0,12)
+$containerID = (docker ps -a |Out-GridView -PassThru).Substring(0, 12)
 docker start -ia $containerID
 
 
-##3 – Dockerize a .NET Console Application
+##3 ï¿½ Dockerize a .NET Console Application
 
 # Move to the project directory.
-cd 'C:\projects\CodeMash2017\Containers for Windows Devs\HelloWorldConsole\HelloWorldConsole\'
+cd 'C:\Projects\ContainersForWindowsDevs\HelloWorldConsole\HelloWorldConsole\'
 
-# Create the image (don’t forget the “.”)
+# Create the image (donï¿½t forget the ï¿½.ï¿½)
 docker build -t helloworldconsole:codemash2017 .
 
 # Show the image just created.
 docker images
 
-# Run the image, specifying the input parameter. Using “--rm” will remove the container after it exits.
-docker run --rm helloworldconsole:codemash2017 “mike”
+# Run the image, specifying the input parameter. Using ï¿½--rmï¿½ will remove the container after it exits.
+docker run --rm helloworldconsole:codemash2017 ï¿½mikeï¿½
 
 # Show the container is not available
 docker ps -a
